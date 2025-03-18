637a7719dc84eec591eb19c0df586ace3935bf80
Yakob <dibabuyakob@gmail.com>
2025-03-18 14:21:27 +0300

    Created the models for my event in the api app, reference to the django built-in user model to form relationship between the event model and the user model.

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateTimeField()
+    location = models.CharField(max_length=200)
+    origanizer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='events')
+    capacity = models.IntegerField()
+    created_date = models.DateField()
